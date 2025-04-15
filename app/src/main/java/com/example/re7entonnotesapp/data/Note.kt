e8e0729158dd81bee739ce19adf093d2cf39351e
Kianvash <Kianvash.ghahari@gmail.com>
2025-04-16 00:33:47 +0330

    Added timestamp, lastEdited properties to Note entity.

--- app/src/main/java/com/example/re7entonnotesapp/data/Note.kt
+++ app/src/main/java/com/example/re7entonnotesapp/data/Note.kt
@@ -8,5 +8,7 @@ data class Note(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
-    val content: String
+    val content: String,
+    val timestamp: Long = System.currentTimeMillis(), // Creation time
+    val lastEdited: Long = System.currentTimeMillis() // Last edited time
 )
