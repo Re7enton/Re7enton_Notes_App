5a10f5a0b54d4cb0ba12f4c5f3894bd60c6301da
Kianvash <Kianvash.ghahari@gmail.com>
2025-04-23 18:47:51 +0330

    trying to fix errors on ktor implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,10 +63,12 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.android)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.work.manager)
 //    implementation(libs.kotlin.kapt)
+    implementation(libs.ksp.api)
     implementation(libs.hilt.work)
     ksp(libs.hilt.compiler)
 
--- app/src/main/java/com/example/re7entonnotesapp/di/KtorClientProvider.kt
+++ app/src/main/java/com/example/re7entonnotesapp/di/KtorClientProvider.kt
@@ -5,11 +5,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
+import io.ktor.client.engine.android.Android
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -18,15 +23,17 @@ object KtorClientProvider {
     @Provides
     @Singleton
     fun provideHttpClient(): HttpClient =
-        HttpClient(CIO) {
+        HttpClient(Android) {
             install(ContentNegotiation) {
-                json(
-                    Json {
-                        prettyPrint = true
-                        isLenient = true
-                        ignoreUnknownKeys = true
-                    }
-                )
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+            defaultRequest {
+                // set these on every request
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
+                header(HttpHeaders.Accept,      ContentType.Application.Json)
             }
         }
 }
